68ef596eec2e1d1d9f0f1993a45add1bda093d3d
Santi <santiasecas@gmail.com>
2018-02-21 14:59:33 +0100

    Tab spacing changed

--- practica1/src/practica1.js
+++ practica1/src/practica1.js
@@ -9,88 +9,88 @@ var MemoryGame = MemoryGame || {};
  * Constructora de MemoryGame
  */
 MemoryGame = function(gs) {
-	this.gs = gs;
-	this.cartas = [];
-	this.cartasEncontradas = [];
-	this.mensajeEstado;
-	
+    this.gs = gs;
+    this.cartas = [];
+    this.cartasEncontradas = [];
+    this.mensajeEstado;
+    
    /**
-	* Inicializa el juego creando las cartas (recuerda que son 2 de cadatipo de carta), 
-	* desordenándolas y comenzando el bucle de juego.
-	*/
-	this.initGame = function() {
-		this.mensajeEstado = "Memory Game";
-		this.crearCartas();
-		this.desordenarCartas();
-		this.loop();
-		
-	}
-	
+    * Inicializa el juego creando las cartas (recuerda que son 2 de cadatipo de carta), 
+    * desordenándolas y comenzando el bucle de juego.
+    */
+    this.initGame = function() {
+        this.mensajeEstado = "Memory Game";
+        this.crearCartas();
+        this.desordenarCartas();
+        this.loop();
+        
+    }
+    
    /**
     * Dibuja el juego, esto es: (1) escribe el mensaje con el estado actual del juego y 
-	* (2) pide a cada una de las cartas del tablero que se dibujen.
-	*/
-	this.draw = function() {
+    * (2) pide a cada una de las cartas del tablero que se dibujen.
+    */
+    this.draw = function() {
 
-	}
-	
+    }
+    
    /**
-	* Es el bucle del juego.
-	*/
-	this.loop = function() {
-		
-	}
-	
+    * Es el bucle del juego.
+    */
+    this.loop = function() {
+        
+    }
+    
    /**
     * Este método se llama cada vez que el jugador pulsa sobre
-	* alguna de las cartas (identificada por el número que ocupan en el array de cartas
-	* del juego). Es el responsable de voltear la carta y, si hay dos volteadas, comprobar
-	* si son la misma (en cuyo caso las marcará como encontradas). En caso de no ser
-	* la misma las volverá a poner boca abajo
-	*/
-	this.onClick = function(cardId) {
-		
-	}
-	
+    * alguna de las cartas (identificada por el número que ocupan en el array de cartas
+    * del juego). Es el responsable de voltear la carta y, si hay dos volteadas, comprobar
+    * si son la misma (en cuyo caso las marcará como encontradas). En caso de no ser
+    * la misma las volverá a poner boca abajo
+    */
+    this.onClick = function(cardId) {
+        
+    }
+    
    /**
-	* Este método crea 8 pares de cartas de las disponibles y las guarda en el array de cartas
-	*/
-	this.crearCartas = function() {
-		this.cartas = [
-			new MemoryGameCard("8-ball"),
-			new MemoryGameCard("8-ball"),
-			new MemoryGameCard("potato"), 
-	    	new MemoryGameCard("potato"),
-	    	new MemoryGameCard("dinosaur"), 
-	    	new MemoryGameCard("dinosaur"),
-	    	new MemoryGameCard("kronos"), 
-	    	new MemoryGameCard("kronos"),
-	    	new MemoryGameCard("rocket"), 
-	    	new MemoryGameCard("rocket"),
-	    	new MemoryGameCard("unicorn"), 
-	    	new MemoryGameCard("unicorn"),
-	    	new MemoryGameCard("guy"), 
-	    	new MemoryGameCard("guy"),
-	    	new MemoryGameCard("zeppelin"), 
-	    	new MemoryGameCard("zeppelin")
-		]
-	}
-	
+    * Este método crea 8 pares de cartas de las disponibles y las guarda en el array de cartas
+    */
+    this.crearCartas = function() {
+        this.cartas = [
+            new MemoryGameCard("8-ball"),
+            new MemoryGameCard("8-ball"),
+            new MemoryGameCard("potato"), 
+            new MemoryGameCard("potato"),
+            new MemoryGameCard("dinosaur"), 
+            new MemoryGameCard("dinosaur"),
+            new MemoryGameCard("kronos"), 
+            new MemoryGameCard("kronos"),
+            new MemoryGameCard("rocket"), 
+            new MemoryGameCard("rocket"),
+            new MemoryGameCard("unicorn"), 
+            new MemoryGameCard("unicorn"),
+            new MemoryGameCard("guy"), 
+            new MemoryGameCard("guy"),
+            new MemoryGameCard("zeppelin"), 
+            new MemoryGameCard("zeppelin")
+        ]
+    }
+    
    /**
-	* Este método desordena las cartas de manera aleatoria
-	*/
-	this.desordenarCartas = function() {
-		var numCartas = this.cartas.length
-		var cartasAux = [];
-		while(numCartas > 0) {
-			elem = Math.floor(Math.random() * numCartas);
-			aux = this.cartas[elem];
-			this.cartas.splice(elem,1);
-			cartasAux.push(aux);
-			numCartas = this.cartas.length;
-		}
-		this.cartas = cartasAux;
-	}
+    * Este método desordena las cartas de manera aleatoria
+    */
+    this.desordenarCartas = function() {
+        var numCartas = this.cartas.length
+        var cartasAux = [];
+        while(numCartas > 0) {
+            elem = Math.floor(Math.random() * numCartas);
+            aux = this.cartas[elem];
+            this.cartas.splice(elem,1);
+            cartasAux.push(aux);
+            numCartas = this.cartas.length;
+        }
+        this.cartas = cartasAux;
+    }
 };
 
 /**
@@ -100,50 +100,50 @@ MemoryGame = function(gs) {
  * @param {string} id Nombre del sprite que representa la carta
  */
 MemoryGameCard = function(id) {
-	
-	this.nombre = id;
-	this.volteada = false;
-	this.encontrada = false;
-	
+    
+    this.nombre = id;
+    this.volteada = false;
+    this.encontrada = false;
+    
    /** 
-	* Da la vuelta a la carta, cambiando el estado de la misma
-	*/
-	this.flip() {
-		if (this.volteada == false) {
-			this.volteada = true;
-		}
-		else { 
-		this.volteada = false;
-		}
-	}
-	
+    * Da la vuelta a la carta, cambiando el estado de la misma
+    */
+    this.flip() {
+        if (this.volteada == false) {
+            this.volteada = true;
+        }
+        else { 
+        this.volteada = false;
+        }
+    }
+    
    /** 
-	* Marca una carta como encontrada, cambiando el estado de la misma
-	*/
-	this.found() {
-		this.encontrada = true;
-	}
-	 
+    * Marca una carta como encontrada, cambiando el estado de la misma
+    */
+    this.found() {
+        this.encontrada = true;
+    }
+     
    /**
-	* Compara dos cartas, devolviendo true si ambas representan la misma carta
-	*/
-	this.compareTo(otherCard) {
-		return this.nombre == otherCard.nombre;
-	}
-	
+    * Compara dos cartas, devolviendo true si ambas representan la misma carta
+    */
+    this.compareTo(otherCard) {
+        return this.nombre == otherCard.nombre;
+    }
+    
    /**
-	* Dibuja la carta de acuerdo al estado en el que se encuentra.
-	* Recibe como parámetros el servidor gráfico y la posición en la que se encuentra en
-	* el array de cartas del juego (necesario para dibujar una carta).
-	*/
-	this.draw(gs, pos) {
-		// Si no está volteada, se ve la cara trasera.
-		if(this.volteada = false) {
-			gs.draw("back",pos);
-		}
-		// En caso contrario, se ve la cara de su id.
-		else {
-			gs.draw(this.nombre,pos);
-		}
-	}
+    * Dibuja la carta de acuerdo al estado en el que se encuentra.
+    * Recibe como parámetros el servidor gráfico y la posición en la que se encuentra en
+    * el array de cartas del juego (necesario para dibujar una carta).
+    */
+    this.draw(gs, pos) {
+        // Si no está volteada, se ve la cara trasera.
+        if(this.volteada = false) {
+            gs.draw("back",pos);
+        }
+        // En caso contrario, se ve la cara de su id.
+        else {
+            gs.draw(this.nombre,pos);
+        }
+    }
 };
